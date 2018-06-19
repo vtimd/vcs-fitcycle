91a357f6a48c5fcad81f8934311525d132434d12
Sean O'Dell <sean.odell@cloudychance.io>
2018-06-19 11:46:00 -0500

    Updates

--- testvpc.tf
+++ testvpc.tf
@@ -5,14 +5,14 @@ variable "cidr" {}
 variable "images" {
   type = "map"
   default = {
-    web="ami-c79c63bf"
-    mngt="ami-7b986703"
-    db="ami-189d6260"
-    dblb="ami-569b642e"
-    app="ami-bf9d62c7"
+    web="ami-31126b4e"
+    mgmt="ami-c1156cbe"
+    db="ami-67156c18"
+    dblb="ami-56146d29"
+    app="ami-9e1c65e1"
+    api="ami-751e670a"
   }
 }
-
 # Create resources for application deployment
 resource "aws_vpc" "vpc_tuto" {
   cidr_block = "${var.cidr}/16"
@@ -22,7 +22,6 @@ resource "aws_vpc" "vpc_tuto" {
     Name = "${var.vpcname}"
   }
 }
-
 resource "aws_subnet" "public_subnet_us_west_1a" {
   vpc_id                  = "${aws_vpc.vpc_tuto.id}"
   cidr_block              = "${var.cidr}/20"
@@ -31,28 +30,23 @@ resource "aws_subnet" "public_subnet_us_west_1a" {
     Name =  "Subnet main"
   }
 }
-
 resource "aws_internet_gateway" "gw" {
   vpc_id = "${aws_vpc.vpc_tuto.id}"
   tags {
         Name = "InternetGateway"
     }
 }
-
 resource "aws_route" "internet_access" {
   route_table_id         = "${aws_vpc.vpc_tuto.main_route_table_id}"
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = "${aws_internet_gateway.gw.id}"
 }
-
 resource "aws_route_table_association" "public_subnet_us_west_1a_association" {
     subnet_id = "${aws_subnet.public_subnet_us_west_1a.id}"
     route_table_id = "${aws_vpc.vpc_tuto.main_route_table_id}"
 }
-
 resource "aws_security_group" "app_sec_group" {
     name = "app_sec_group"
-
     ingress {
         from_port   = 22
         to_port     = 22
@@ -91,8 +85,7 @@ resource "aws_security_group" "app_sec_group" {
     }
     vpc_id = "${aws_vpc.vpc_tuto.id}"
 }
-
-resource "aws_instance" "Db1" {
+resource "aws_instance" "db1" {
   ami           = "${var.images["db"]}"
   instance_type = "t2.micro"
   subnet_id = "${aws_subnet.public_subnet_us_west_1a.id}"
@@ -100,12 +93,23 @@ resource "aws_instance" "Db1" {
   key_name                    = "${var.keypair}"
   tags {
         App = "${var.vpcname}"
-        Name = "Db1"
+        Name = "db1-${var.vpcname}"
         Tier = "DB"
   }
 }
-
-resource "aws_instance" "DbLb" {
+resource "aws_instance" "db2" {
+  ami           = "${var.images["db"]}"
+  instance_type = "t2.micro"
+  subnet_id = "${aws_subnet.public_subnet_us_west_1a.id}"
+  vpc_security_group_ids = ["${aws_security_group.app_sec_group.id}"]
+  key_name                    = "${var.keypair}"
+  tags {
+        App = "${var.vpcname}"
+        Name = "db2-${var.vpcname}"
+        Tier = "DB"
+  }
+}
+resource "aws_instance" "dblb" {
   ami           = "${var.images["dblb"]}"
   instance_type = "t2.micro"
   subnet_id = "${aws_subnet.public_subnet_us_west_1a.id}"
@@ -113,12 +117,11 @@ resource "aws_instance" "DbLb" {
   key_name                    = "${var.keypair}"
   tags {
         App = "${var.vpcname}"
-        Name = "DB Load Balancer"
+        Name = "dblb-${var.vpcname}"
         Tier = "DBLB"
   }
 }
-
-resource "aws_instance" "App1" {
+resource "aws_instance" "app1" {
   ami           = "${var.images["app"]}"
   instance_type = "t2.micro"
   subnet_id = "${aws_subnet.public_subnet_us_west_1a.id}"
@@ -126,12 +129,11 @@ resource "aws_instance" "App1" {
   key_name                    = "${var.keypair}"
   tags {
         App = "${var.vpcname}"
-        Name = "App1"
+        Name = "app1-${var.vpcname}"
         Tier = "App"
   }
 }
-
-resource "aws_instance" "App2" {
+resource "aws_instance" "app2" {
   ami           = "${var.images["app"]}"
   instance_type = "t2.micro"
   subnet_id = "${aws_subnet.public_subnet_us_west_1a.id}"
@@ -139,12 +141,11 @@ resource "aws_instance" "App2" {
   key_name                    = "${var.keypair}"
   tags {
         App = "${var.vpcname}"
-        Name = "App2"
+        Name = "app2-${var.vpcname}"
         Tier = "App"
   }
 }
-
-resource "aws_instance" "App3" {
+resource "aws_instance" "app3" {
   ami           = "${var.images["app"]}"
   instance_type = "t2.micro"
   subnet_id = "${aws_subnet.public_subnet_us_west_1a.id}"
@@ -152,12 +153,11 @@ resource "aws_instance" "App3" {
   key_name                    = "${var.keypair}"
   tags {
         App = "${var.vpcname}"
-        Name = "App3"
+        Name = "app3-${var.vpcname}"
         Tier = "App"
   }
 }
-
-resource "aws_instance" "Web1" {
+resource "aws_instance" "web1" {
   ami           = "${var.images["web"]}"
   instance_type = "t2.micro"
   subnet_id = "${aws_subnet.public_subnet_us_west_1a.id}"
@@ -165,12 +165,11 @@ resource "aws_instance" "Web1" {
   key_name                    = "${var.keypair}"
   tags {
         App = "${var.vpcname}"
-        Name = "Web1"
+        Name = "web1-${var.vpcname}"
         Tier = "Web"
   }
 }
-
-resource "aws_instance" "Web2" {
+resource "aws_instance" "web2" {
   ami           = "${var.images["web"]}"
   instance_type = "t2.micro"
   subnet_id = "${aws_subnet.public_subnet_us_west_1a.id}"
@@ -178,33 +177,31 @@ resource "aws_instance" "Web2" {
   key_name                    = "${var.keypair}"
   tags {
         App = "${var.vpcname}"
-        Name = "Web2"
+        Name = "web2-${var.vpcname}"
         Tier = "Web"
   }
 }
-
-resource "aws_instance" "Db2" {
-  ami           = "${var.images["db"]}"
+resource "aws_instance" "api" {
+  ami           = "${var.images["api"]}"
   instance_type = "t2.micro"
   subnet_id = "${aws_subnet.public_subnet_us_west_1a.id}"
   vpc_security_group_ids = ["${aws_security_group.app_sec_group.id}"]
   key_name                    = "${var.keypair}"
   tags {
         App = "${var.vpcname}"
-        Name = "Db2"
-        Tier = "DB"
+        Name = "api-${var.vpcname}"
+        Tier = "API"
   }
 }
-
-resource "aws_instance" "Mngt" {
-  ami           = "${var.images["mngt"]}"
-  instance_type = "t2.micro"
+resource "aws_instance" "mgmt" {
+  ami           = "${var.images["mgmt"]}"
+  instance_type = "t2.medium"
   subnet_id = "${aws_subnet.public_subnet_us_west_1a.id}"
   vpc_security_group_ids = ["${aws_security_group.app_sec_group.id}"]
   key_name                    = "${var.keypair}"
   tags {
         App = "${var.vpcname}"
-        Name = "Management"
-        Tier = "Mngt"
+        Name = "mgmt-${var.vpcname}"
+        Tier = "Mgmt"
   }
 }
